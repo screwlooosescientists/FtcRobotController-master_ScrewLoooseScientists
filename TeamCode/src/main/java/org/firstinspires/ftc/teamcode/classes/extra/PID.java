325ad871f6ec0c31cc2ba1de4a6744ade8d15154
Ftc sls <thomaskamperman@gmail.com>
2024-01-26 22:12:21 +0100

    Added tenserflow model to asset folder

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/extra/PID.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/classes/extra/PID.java
@@ -6,12 +6,16 @@ import java.util.Timer;
 
 public class PID {
 
+    //The gains for the pid loop, kp: proportional, ki: integral, Kd: derivative
     public static double kp;
     public static double ki;
     public static double kd;
+
+    //Target and current data,
+    public double targetValue, atThisExactMomentValue;
     ElapsedTime timer = new ElapsedTime();
 
-    public PID(double kp, double ki, double kd)
+    public PID(double kp, double ki, double kd) //Constructor to create an pid loop :)
     {
         this.kp = kp;
         this.ki = ki;
