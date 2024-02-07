eed3338c0e9a3fa3710f891b102930a4dfd54973
ThomasKamperman <73964867+ThomasKamperman@users.noreply.github.com>
2024-02-07 10:23:16 +0100

    Update Window.java
    Added the code to generate an window using java.awt and java.swing

--- TrackTracer/Graphics/Window.java
+++ TrackTracer/Graphics/Window.java
@@ -5,7 +5,16 @@ public class Window {
 
     public static void CreateWindow()
     {
-        
+        JFrame frame = new JFrame("TrackTracer");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+ 
+        JLabel emptyLabel = new JLabel("");
+        emptyLabel.setPreferredSize(new Dimension(175, 100));
+        frame.getContentPane().add(emptyLabel, BorderLayout.CENTER);
+ 
+        //Display the window.
+        frame.pack();
+        frame.setVisible(true);
     }
 
 }
